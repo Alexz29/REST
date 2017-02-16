f997fef71ff9fa964c171dfb681756c943e8ef52
alexey <Alexz29_MAC>
2017-02-16 17:45:00 +0300

    Update method delete pk from post, put

--- Rest/Methods/Update.php
+++ Rest/Methods/Update.php
@@ -5,20 +5,26 @@
  */
 
 namespace Rest\Methods;
+use Common\Request;
 use Rest\Serializer;
 
 class Update extends BaseMethod
 {
-    public function index($model){
-        if(!isset($this->getRequest['id'])){
-            throw new \Exception("Require param id not found!");
-        }
+    public function index($model)
+    {
+        $request=Request::getData('get');
 
-        $model=$model::find($this->getRequest['id']);
-//        $model->attributes=$this->postRequest;
-        $model->attributes=$this->getRequest;
+        //require get param id
+        if(!isset($request['id']))
+            throw new \Exception("Require param id not found!");
+
+        $data = (Request::getData('post') ? Request::getData('post') : Request::getData('put'));
+        unset($data[$model::$primary_key]);
+
+        $model=$model::find($request['id']);
+        $model->attributes=$data;
         $model->save();
 
-        echo Serializer::model($model, $this->format);
+        echo Serializer::model($model);
     }
 }
